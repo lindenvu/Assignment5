b67903327a1739b8d6da0041f10b9877825481c2
Linden Vu <github@lindenvu.com>
2018-03-22 17:30:32 -0700

    Update 1.2

--- js/employee-management.js
+++ js/employee-management.js
@@ -1,2 +1,10 @@
 /*eslint-env browser*/
 
+var employee_list = [["Zak Ruvalcaba", "Engineering", 3333],
+                 ["Sally Smith", "Quality Assurance", 3424],
+                 ["Fred Franklin", "Sales", 3346],
+                 ["John Smith", "Marketing", 3232],
+                 ["Jane Caruthers", "Finance", 3451]];
+
+window.document.write(employee_list[0][0]);
+
